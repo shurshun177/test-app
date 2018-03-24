746a65ef3818222e928d07d4ebcec29d12deffc3
shur <shur@shurbuntu.com>
2018-03-25 00:29:32 +0300

    implemented adding and editing expense model

--- MyExpenses/urls.py
+++ MyExpenses/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.expense_list, name='expense_list'),
+    url(r'^delete/(?P<pk>\d+)/$', views.ExpenseDelete.as_view(),
+        name='expense_delete'),
+    url(r'^expense/new/$', views.expense_new, name='expense_new'),
+    url(r'^expense/(?P<pk>\d+)/$', views.expense_detail, name='expense_detail'),
+    url(r'^expense/(?P<pk>\d+)/edit/$', views.expense_edit, name='expense_edit')
 ]
